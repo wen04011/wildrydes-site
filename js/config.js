639de02d93b172e4cd07b0c19a52a75482e5aeee
wen04011 <wen04011@gmail.com>
2021-09-09 15:48:25 +0800

    modify index.html again

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_MlUOBQoeI', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1tokajdku9b2u6mmehf2pngtoi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4avsbf9ucp56rs72vehuoo060i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
